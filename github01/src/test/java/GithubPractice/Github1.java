9f0927a67ed8e76f74b1f541afbda0298dce7737
sushant <sushantmech384@gmail.com>
2025-07-08 16:53:36 +0530

    change 2nd commit

--- github01/src/test/java/GithubPractice/Github1.java
+++ github01/src/test/java/GithubPractice/Github1.java
@@ -5,6 +5,7 @@ public class Github1 {
 	public static void main(String[] args) {
 		System.out.println("push the code on github");
 		System.out.println("send code github to system");
+		System.out.println("push the code after change");
 
 	}
 
