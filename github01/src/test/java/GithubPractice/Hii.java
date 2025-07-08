6c5bda6019126147de80a54e92c99746dab10f55
sushant <sushantmech384@gmail.com>
2025-07-08 17:27:54 +0530

    added

---
+++ github01/src/test/java/GithubPractice/Hii.java
@@ -0,0 +1,15 @@
+package GithubPractice;
+
+public class Hii {
+
+	public static void main(String[] args) {
+		
+			System.out.println("hiiii");
+			System.out.println("hiiiihhhhhhh");
+
+		}
+
+
+	}
+
+
